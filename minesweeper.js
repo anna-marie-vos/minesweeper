f9a793100c5365276cbb10c09dd67e9fd2da0d84
anna-marie-vos <amvos@outlook.com>
2016-10-31 21:27:05 +1300

    attemp1 + 2

--- minesweeper.js
+++ minesweeper.js
@@ -2,10 +2,22 @@ document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
 var board = {};
- board.cells = ['a',
- {row:0, col:0, isMine:0, hidden:0},
- {row:1, col:1, isMine:0,hidden:{}},
- {row:2, col:2, isMine:0, hidden:{}}];
+// Attempt 1
+ board.cells = [
+   {row:0, col:0, isMine:0, hidden:0},
+   {row:1, col:1, isMine:0, hidden:0},
+   {row:2, col:2, isMine:0, hidden:0},
+   {row:3, col:3, isMine:0, hidden:0}];
+
+//Attempt 2
+// board.cells = [c1,c2,c3,c4];
+// var c1 = {};
+// var c2 = {};
+// var c3 = {};
+// var c4 = {};
+
+
+
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
