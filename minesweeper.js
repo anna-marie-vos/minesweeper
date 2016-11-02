575ef153e26152a64e93b82dc73974674a8b4494
anna-marie-vos <amvos@outlook.com>
2016-11-03 09:23:13 +1300

    re-save

--- minesweeper.js
+++ minesweeper.js
@@ -32,7 +32,7 @@ function startGame () {
 function checkForWin () {
   var totalCount = 0;
   var markedCount = 0;
-  for(var x =0; x < board.cells.length; x++){
+  for(var x =0 ; x < board.cells.length; x++){
     if (board.cells[x].isMine === true){
       totalCount +=1;
     } else {totalCount +=0;}
@@ -40,7 +40,7 @@ function checkForWin () {
       markedCount +=1;
     } else{markedCount +=0;}
   }
-if(totalCount == markedCount){
+if(totalCount === markedCount){
   return lib.displayMessage('You win!');
 } else {
   return ;
